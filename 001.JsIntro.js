e9b8d41dc72c295030e9ee17a3ffd3f921928ca6
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-06-18 08:28:04 +0530

    Create 001.JsIntro.js

---
+++ 001.JsIntro.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+<p  id="demo1">Hi,Ch dhanush</p>
+<button type="button" onclick='document.getElementById("demo1").innerHTML ="Hello Good morning!"'>Click..! </button>
+
+</body>
+</html>
