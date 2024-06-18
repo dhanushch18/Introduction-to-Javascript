5627485b4a9783da88a8fddab9c733e6b89bec98
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-06-18 22:38:30 +0530

    Create 003.JSButton2.js

---
+++ 003.JSButton2.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+  document.getElementById("demo1").innerHTML ="ch dhanush.";
+}
+</script>
+</head>
+<body>
+
+<h2>Demo JavaScript in Head</h2>
+
+<p id="demo">A Paragraph.</p>
+<p id="demo1">dhanush ch 17</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+</body>
+</html> 
